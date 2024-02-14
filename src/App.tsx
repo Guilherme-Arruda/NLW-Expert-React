09be4de1ebb9d05bdb91be73d6dbdd7144448fb6
Guilherme Arruda <arruda.guilherme@outlook.com>
2024-02-14 16:00:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export const App = () => {
         <input
           type='text'
           placeholder='Busque em suas notas...'
-          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          className='w-full bg-transparent text-xl sm:text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
           onChange={handleSearch}
         />
       </form>
